613bddd63e1af3ebf5e3527afda95f987255de9b
leonardo <leonardoschmabreu@gmail.com>
2021-05-28 14:34:13 -0300

    creating all classes and interfaces

---
+++ Player.java
@@ -0,0 +1,12 @@
+
+public abstract class Player {
+
+    public float energy;
+    public float power;
+
+    public float getEnergy(){ return energy; }
+    public float getPower(){ return power; }
+    public void setEnergy(float energy) { this.energy = energy; }
+    public void setPower(float power) { this.power = power; }
+
+}
